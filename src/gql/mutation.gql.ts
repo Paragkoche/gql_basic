4feda61e0a7ee9ffdc59d2978c957193284b9879
Your Name <you@example.com>
2024-07-20 19:52:51 +0530

    update

--- src/gql/mutation.gql.ts
+++ src/gql/mutation.gql.ts
@@ -1,4 +1,4 @@
-import { GraphQLObjectType, GraphQLString } from "graphql";
+import { GraphQLID, GraphQLObjectType, GraphQLString } from "graphql";
 import { todo } from "../db/todo.gql";
 import { GraphQLDate } from "graphql-iso-date";
 import { DB_client } from "../db/client.db";
@@ -21,6 +21,46 @@ export const mutation = new GraphQLObjectType({
           data: {
             completeOn: args.completeOn,
             message: args.message,
+          },
+        });
+      },
+    },
+
+    updateTodo: {
+      type: todo,
+      args: {
+        id: {
+          type: GraphQLID,
+        },
+        message: {
+          type: GraphQLString,
+        },
+        completeOn: {
+          type: GraphQLDate,
+        },
+      },
+      resolve: async (_, args) => {
+        return await DB_client.todos.update({
+          where: { id: args.id },
+          data: {
+            completeOn: args.completeOn,
+            message: args.message,
+          },
+        });
+      },
+    },
+
+    deleteTodo: {
+      type: todo,
+      args: {
+        id: {
+          type: GraphQLID,
+        },
+      },
+      resolve: async (_, args) => {
+        return await DB_client.todos.delete({
+          where: {
+            id: args.id,
           },
         });
       },
